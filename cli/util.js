0ab73bc92ae031e4ea4f1dc8769c32d1033bfcc1
David Reed <x@sys32.dev>
2022-04-20 21:16:13 -0400

    confirm only deletion

--- cli/util.js
+++ cli/util.js
@@ -1,6 +1,31 @@
 import promptly from 'promptly';
 import Server from '../Server.js';
 
+/**
+ *
+ * @param {import('../Server.js').default} server
+ * @param {string|number} i
+ * @returns
+ */
+async function resolve_id(server, i, confirm) {
+	if (!isNaN(i)) {
+		const id = await server.id_at_index(i);
+		console.table(await server.show_game(id));
+		if (
+			confirm !== true ||
+			(await promptly.confirm('Is this the correct game? (y/n)'))
+		) {
+			return id;
+		} else {
+			process.exit();
+		}
+	} else if (typeof i === 'string') {
+		return i;
+	} else {
+		throw new TypeError(`Unknown ID type ${typeof i}`);
+	}
+}
+
 export async function list_games() {
 	const server = new Server();
 
@@ -26,34 +51,12 @@ export async function show_game(id) {
 	console.table(await server.show_game(id));
 }
 
-/**
- *
- * @param {import('../Server.js').default} server
- * @param {string|number} i
- * @returns
- */
-async function resolve_id(server, i) {
-	if (!isNaN(i)) {
-		const id = await server.id_at_index(i);
-		console.table(await server.show_game(id));
-		if (await promptly.confirm('Is this the correct game? (y/n)')) {
-			return id;
-		} else {
-			process.exit();
-		}
-	} else if (typeof i === 'string') {
-		return i;
-	} else {
-		throw new TypeError(`Unknown ID type ${typeof i}`);
-	}
-}
-
 export async function delete_game(id) {
 	const server = new Server();
 
 	await server.open;
 
-	id = await resolve_id(server, id);
+	id = await resolve_id(server, id, true);
 
 	const deleted = await server.delete_game(id);
 
