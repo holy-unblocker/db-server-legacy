3965659a7adeca0d21fe656c47dafc132a34c1de
David Reed <reedswork@proton.me>
2022-11-15 17:23:08 -0500

    fix typo

--- src/collectENV.ts
+++ src/collectENV.ts
@@ -5,6 +5,6 @@ expand(config());
 
 export const cfEmail: string = process.env.CF_EMAIL!;
 export const cfKey: string = process.env.CF_KEY!;
-export const namesiloKey: string = process.env.NAMESILO1!;
+export const namesiloKey: string = process.env.NAMESILO!;
 export const nameserver1: string = process.env.NS1!;
 export const nameserver2: string = process.env.NS2!;
