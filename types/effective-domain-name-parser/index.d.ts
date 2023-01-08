5e423b34d7c0c2708e7add94c8aba8ba34655799
David Reed <reedswork@proton.me>
2023-01-08 15:14:38 -0500

    fix type

--- types/effective-domain-name-parser/index.d.ts
+++ types/effective-domain-name-parser/index.d.ts
@@ -1,7 +1,5 @@
-declare module 'effective-domain-name-parser' {
-	export function parse(name: string): {
-		tld: string;
-		sld: string;
-		subdomain: string;
-	};
-}
+export function parse(name: string): {
+	tld: string;
+	sld: string;
+	subdomain: string;
+};
