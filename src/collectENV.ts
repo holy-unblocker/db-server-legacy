61711f4611ae3b938622b6bdd954cf5fe0b1e73f
David Reed <reedswork@proton.me>
2023-01-09 19:28:32 -0500

    fix env

--- src/collectENV.ts
+++ src/collectENV.ts
@@ -9,4 +9,4 @@ export const namesiloKey: string = process.env.NAMESILO!;
 export const nameserver1: string = process.env.NS1!;
 export const nameserver2: string = process.env.NS2!;
 // for debugging without spending money
-export const skipPayment: boolean = process.env.SKIP_PAYMENT !== '1';
+export const skipPayment: boolean = process.env.SKIP_PAYMENT === '1';
