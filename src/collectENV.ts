4932d7eb4d20a739a14020b0c4ef2c27f31906c4
David Reed <reedswork@proton.me>
2023-04-02 13:49:32 -0400

    remove unused env, cleanup

--- src/collectENV.ts
+++ src/collectENV.ts
@@ -3,10 +3,5 @@ import { config } from 'dotenv-flow';
 
 expand(config());
 
-export const cfEmail: string = process.env.CF_EMAIL!;
-export const cfKey: string = process.env.CF_KEY!;
-export const namesiloKey: string = process.env.NAMESILO!;
-export const nameserver1: string = process.env.NS1!;
-export const nameserver2: string = process.env.NS2!;
-// for debugging without spending money
-export const skipPayment: boolean = process.env.SKIP_PAYMENT === '1';
+export const pgURL = process.env.PG_URL || '';
+if (!pgURL) throw new TypeError('Postgresql connection URI required (PG_URL)');
--- src/dbConnect.ts
+++ src/dbConnect.ts
@@ -1,7 +1,8 @@
+import { pgURL } from './collectENV.js';
 import pg from 'pg';
 
 const dbConnect = async () => {
-	const client = new pg.Client({ connectionString: process.env.PG });
+	const client = new pg.Client({ connectionString: pgURL });
 
 	await client.connect();
 
